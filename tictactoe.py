acb6c569d869e82837105f3b25271ac8144599e8
ConnorIsBored <Connorhefley2009@gmail.com>
2025-07-15 03:25:42 -0500

    tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -1,22 +1,22 @@
-board = [""for_in range(9)]
+board = [" "for _ in range(9)]
 
 def print_board():
     print()
     for i in range(3):
-        print(f'{board[i*3]} |{board[i*3+1]} | {board[i*3+2]}')
-        if i <2:
-            print('---|---|---')
+        print(f'{board[i * 3]}    | {board[i * 3 + 1]}    |  {board[i * 3 + 2]}')
+        if i < 2:
+            print(' --- | --- | --- ')
     print()
 
 def get_player_move(player):
     while True:
         try:
-            move =int(input(f'player{player}, choose a position (1-9.')) -1
+            move =int(input(f'player{player}, choose a position (1-9).')) -1
 
-            if move is >= 0 and move <9 and board[move] ==''
-                return move.
+            if move >= 0 and move <9 and board[move] ==' ':
+                return move
             else:
-                print('invalid move cell is taken or out of range').
+                print('invalid move cell is taken or out of range')
         except ValueError:
                 print('try again prick (1-9')
 
@@ -30,28 +30,28 @@ def check_winner(player):
     for combo in win_combo:
         if all(board[i] ==player for i in combo):
             return True
-        return False
+    return False
 
 def is_draw():
-    print('Draw!')
-    return ''not in board
+    return ' 'not in board
 
 def play_game():
     current_player ='X'
-    While True:
+    while True:
         print_board()
         move = get_player_move(current_player)
         board[move] = current_player
 
-    if check_winner(current-player):
-        print board()
-        print(f'Player {current_player} wins!")
+        if check_winner(current_player):
+            print_board()
+            print(f'Player {current_player} wins!')
 
-    elif is_draw():
-        print board()
-        break
+        elif is_draw():
+            print('Draw!')
+            print_board()
+            break
 
-        current_player = 'O' if current player =='X' else 'X'
+        current_player = 'O' if current_player =='X' else 'X'
 
-if__name__is=='__main__':
+if __name__=='__main__':
     play_game()
