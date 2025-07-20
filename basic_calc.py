891377cfcb3f373d2bf790ad50914d03ed6019f0
luci <gfuonxner@gmail.com>
2025-07-20 00:33:58 -0500

    made the calculator replayable

--- basic_calc.py
+++ basic_calc.py
@@ -20,3 +20,10 @@ def basic_calc():
     else:
         print(f"Error: '{op}' is not a valid operator.")
 
+while True:
+    basic_calc()
+
+    replay = input('do you want to go again? (Y/N): ').strip().upper()
+    if replay not in ['y','yes']:
+        print('goodbye')
+        break
