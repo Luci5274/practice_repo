d7156a69ff54aed54d8d991167396d8c4fc7d9f4
Luci5274 <126932099+Luci5274@users.noreply.github.com>
2025-07-20 17:00:50 -0500

    started building smaller text adventure

---
+++ text_adv/simpler_TA_game/Simple_TA_game.py
@@ -0,0 +1,13 @@
+def first_room():
+
+
+def main():
+    print('welcome to text adventure')
+    name = input('what is your name?: ')
+    print(f'welcome {name} its time to begin....\n')
+
+
+if __name__ == "__main__":
+    main()
+
+
